9adf46a6c6dc2ec2d899a0c042aac29ba6707d9c
Artur Mostowski <vulwsztyn@gmail.com>
2021-12-14 18:56:46 +0100

    Day 1 - Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -13,12 +13,19 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        for ((index, line: String) in input.drop(3).iterator().withIndex()) {
+            if (line.toInt() > input[index].toInt()) {
+                count += 1
+            }
+        }
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
