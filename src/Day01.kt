c25b6e8785f9d9b43c909b38e7a1a17b41b17bce
Artur Mostowski <artur.mostowski@currency-one.com>
2021-12-14 17:54:20 +0100

    Day 1 - Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,15 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var last = input[0].toInt()
+        for (line: String in input.drop(1)) {
+            val current = line.toInt()
+            if (current > last) {
+                count += 1
+            }
+            last = current
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +18,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
